96e47ba8804dc00f29dbd047c23434fcc2b14e48
longlg88 <longlg88@naver.com>
2019-05-15 16:39:00 +0900

    first test

--- test.py
+++ test.py
@@ -5,8 +5,9 @@ import subprocess
 
 if __name__ == "__main__":
     # result = subprocess.check_output("kubectl get pv --all-namespaces -o json | jq '.items[].status.phase'", shell=True)
-    result = os.system("kubectl get pv --all-namespaces -o json | jq '.items[].status.phase'")
-    print("here : "+str(result))
+    result = subprocess.run("kubectl get pv --all-namespaces -o json | jq '.items[].status.phase'", stdout=subprocess.PIPE)
+    print("here : "+result.stdout.decode('utf-8'))
+    result=result.stdout.decode('utf-8')
     result=result.replace('"','')
     print(result.split("\n"))
     a=0
@@ -17,6 +18,7 @@ if __name__ == "__main__":
             a=str(a)
             cmd = "kubectl get pv --all-namespaces -o json | jq '.items[" + a + "].spec.claimRef.name'"
             # name = subprocess.check_output(cmd, shell=True)
-            name = os.system(cmd)
+            name = subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode('utf-8')
+            # name = os.system(cmd)
             print(name)
             a=int(a)
