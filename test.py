1a185f539fd4f32d4d83e1821bda5920cebecaa1
longlg88 <longlg88@naver.com>
2019-05-15 15:56:24 +0900

    first test 3

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ import os
 import sys
 
 if __name__ == "__main__":
-    result = os.system("k get pv --all-namespaces -o json | jq '.items[].status.phase'")
+    result = os.system("kubectl get pv --all-namespaces -o json | jq '.items[].status.phase'")
     print(result)
