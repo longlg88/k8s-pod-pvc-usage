2a80b16879313cf2ada8378426161cfbca034238
longlg88 <longlg88@naver.com>
2019-05-15 16:25:59 +0900

    first test

--- test.py
+++ test.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
     print(result.split("\n"))
     a=0
     result_list=result.split("\n")
-    # for s in result_list:
-    index_val=result_list.index("Bound")
-    print(index_val)
-        # if "Bound" in s:
-        #     s.index
-        # else:
+    for s in result_list:
+        print(type(s))
+        if "Bound" == s:
+            a=a+1
+            name = subprocess.check_output("kubectl get pv --all-namespaces -o json | jq '.items[",a,"].spec.claimRef.name")
+            print(name)
