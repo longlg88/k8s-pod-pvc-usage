309259f14c86ec12f884bfbb55464f597357c303
longlg88 <longlg88@naver.com>
2019-05-15 17:58:46 +0900

    first test

--- test.py
+++ test.py
@@ -7,16 +7,17 @@ if __name__ == "__main__":
     result = subprocess.check_output("kubectl get pv --all-namespaces -o json | jq '.items[].status.phase'", shell=True)
     result=result.replace('"','')
     a=0
-    result_list=result.split("\n")
+    result_list=result.rstrip()
     namespace_list=[]
     for s in result_list:
         ## check if pv is "Bound"
         if "Bound" == s:
             a=a+1
             a=str(a)
+            
+            
             ## find name / namespace for pv which is "Bound"
             name_cmd = "kubectl get pv --all-namespaces -o json | jq '.items[" + a + "].spec.claimRef.name'"
-            # name = subprocess.check_output(name_cmd, shell=True).replace('\n','')
             name = subprocess.check_output(name_cmd, shell=True).rstrip()
             namespace_cmd = "kubectl get pv --all-namespaces -o json | jq '.items[" + a + "].spec.claimRef.namespace'"
             namespace = subprocess.check_output(namespace_cmd, shell=True)
@@ -29,25 +30,21 @@ if __name__ == "__main__":
     mount_pod_list=[]
     mount_path_list=[]
     namespace_list = list(set(namespace_list))
+    
+    ## find pod mounted
     for val in namespace_list:
-        ## find pod mounted
         print("val = "+val)
         mount_pod_cmd = "kubectl describe pvc -n " + val + " | grep Mounted | awk '{print $3}'"
         mount_pod = subprocess.check_output(mount_pod_cmd, shell=True)
-        # print(mount_pod)
-        mount_pod=mount_pod.split("\n")
+        mount_pod=mount_pod.rstrip()
 
+        ## find mountPath in pod
         for m_path in mount_pod:
-            ## find mountPath in pod
             if m_path != "<none>" and m_path:
-                # if m_path:
-                #mount_pod_list.append(mount_pod)
                 mount_path_cmd = "kubectl get pod -n " + val + " " + m_path + " -o json | jq '.spec.containers[0].volumeMounts[].mountPath'"
                 mount_path=subprocess.check_output(mount_path_cmd, shell=True)
                 ## append list in list
                 mount_path_temp_list=mount_path.rstrip().split("\n")
-                # mount_path=mount_path.replace("\n","")
-                # mount_path_list.append(mount_path)
                 mount_path_list.append(mount_path_temp_list)
             else:
                 continue
