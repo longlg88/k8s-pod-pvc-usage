81d5c4ff29227bb01da51a63f1c325b8db9f322d
longlg88 <longlg88@naver.com>
2019-05-15 15:53:59 +0900

    first test

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+import os
+import sys
 
 if __name__ == "__main__":
-    
+    result = os.system("k get pv --all-namespaces -o json | jq '.items[].status.phase'")
+    print(result)
