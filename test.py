5ecc5bb0012d2b435e150a5a960592576cca2ce5
longlg88 <longlg88@naver.com>
2019-05-15 16:02:38 +0900

    first test 7

--- test.py
+++ test.py
@@ -6,4 +6,4 @@ import subprocess
 if __name__ == "__main__":
     result = subprocess.check_output("kubectl get pv --all-namespaces -o json | jq '.items[].status.phase'", shell=True)
     print("here : "+result)
-    print(type(result))
+    print(result.split("\n"))
