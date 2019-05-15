d87073b9e3a4f8f1ba3f4791a36160194eca4e4f
longlg88 <longlg88@naver.com>
2019-05-15 16:09:43 +0900

    first test 8

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     result = subprocess.check_output("kubectl get pv --all-namespaces -o json | jq '.items[].status.phase'", shell=True)
     print("here : "+result)
     print(result.split("\n"))
-    result_list=result.split("\n")
+    result_list=result.split("\n").split('\"')
     a=0
     if "Bound" in result_list:
         a=a+1
