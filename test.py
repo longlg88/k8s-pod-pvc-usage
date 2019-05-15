778d37ba9a31856bd939a296f38a57974118bcfc
longlg88 <longlg88@naver.com>
2019-05-15 16:21:25 +0900

    first test

--- test.py
+++ test.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
     print(result.split("\n"))
     a=0
     result_list=result.split("\n")
-    for s in result_list:
-        index_val=s.index("Bound")
-        print(index_val)
+    # for s in result_list:
+    index_val=result_list.index("Bound")
+    print(index_val)
         # if "Bound" in s:
         #     s.index
         # else:
