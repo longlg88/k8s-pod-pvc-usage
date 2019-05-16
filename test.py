bc4216d241f8c5ea39c3798d67a0baf335aa78cc
longlg88 <longlg88@naver.com>
2019-05-16 09:39:26 +0900

    first test

--- test.py
+++ test.py
@@ -45,8 +45,8 @@ if __name__ == "__main__":
                 ## append list in list
                 for xx in mount_path.split("\n"):
                     if "secrets" not in xx and xx and "yaml" not in xx and "ini" not in xx and "toml" not in xx and "config" not in xx:
-                        print(xx)
-                        mount_size_cmd = "kubectl exec -it -n " + val + " " + m_path + " -- /usr/bin/du -c -hs " + xx
+                        print(xx+"*")
+                        mount_size_cmd = "kubectl exec -it -n " + val + " " + m_path + " -- /usr/bin/du -c -hs " + xx + "*"
                         mount_size = subprocess.check_output(mount_size_cmd, shell=True)
                         print(mount_size)
                         # mount_path_temp_list=xx.rstrip()
